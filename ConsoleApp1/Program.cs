bd209a30c887f4f09b60f2f861b355383cf0d79d
oriapp <37003700+oriapp@users.noreply.github.com>
2018-11-01 01:03:53 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello class!");
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("I'm here!");
+            System.Threading.Thread.Sleep(399);
+            Console.WriteLine("(ツ)");
+
+        }
+    }
+}
